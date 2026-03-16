11dacaf2b191afc54e97c5d1ec1ae410b69c9833
友人 <lijunpg@foxmail.com>
2026-03-16 16:02:26 +0800

    feat:前端框架初始化完毕，引入了tailwindcss

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="text-red-500 text-2xl font-bold">hello world</div>
+  <div class="text-amber-300">你好，世界</div>
 </template>
