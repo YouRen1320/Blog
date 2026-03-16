b249316bc1cc3b40683010d6d877a7c770e70040
YouRen1320 <95892717+YouRen1320@users.noreply.github.com>
2026-03-16 16:02:26 +0800

    feat:前端框架初始化完毕，引入了tailwindcss

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -5,4 +5,19 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  postHello(): any {
+    return [
+      {
+        id: 1,
+        name: '第一篇文章',
+        description: '这是2026年3月16日发表的第一篇测试文章',
+      },
+      {
+        id: 2,
+        name: '这是第二篇测试文章',
+        description: '这是2026年3月16日发表的第二篇测试文章',
+      },
+    ];
+  }
 }
