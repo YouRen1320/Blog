370235c11ec93a7b11998e936c0088a2a92f999f
YouRen1320 <95892717+YouRen1320@users.noreply.github.com>
2026-03-19 23:33:13 +0800

    feat:首页第一版完成

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <PageHeader />
   <router-view></router-view>
+  <PageFooter />
 </template>
 
 <script setup lang="ts">
 import PageHeader from '@/views/HomeView/PageHeader.vue'
+import PageFooter from './views/HomeView/PageFooter.vue';
 
 </script>
